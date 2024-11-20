d98d9ad909986757801d0179a0d91fe790856ada
M B Ashish <mbashish007@gmail.com>
2024-11-20 23:08:57 +0530

    Added pom, YummyApplication, CustomerRequest

---
+++ Yummy/src/test/java/com/esd/Yummy/YummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esd.Yummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
