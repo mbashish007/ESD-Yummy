36759b5b4d115720565497c51479e5f0d8583ca3
M B Ashish <mbashish007@gmail.com>
2024-11-20 23:13:17 +0530

    Added EncryptionUtility to encrypt password to Hash

---
+++ Yummy/src/main/java/com/esd/Yummy/utility/EncryptionUtility.java
@@ -0,0 +1,21 @@
+package com.esd.Yummy.utility;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@RequiredArgsConstructor
+public class EncryptionUtility {
+    private final PasswordEncoder passwordEncoder;
+
+    public String encode(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public boolean validates(String password, String encodedPassword) {
+        return passwordEncoder.matches(password, encodedPassword);
+    }
+}
