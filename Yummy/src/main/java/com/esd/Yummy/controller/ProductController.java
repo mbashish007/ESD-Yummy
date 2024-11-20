4283882c9a7c31a6bcc31af013595aa9fba00596
M B Ashish <mbashish007@gmail.com>
2024-11-21 00:04:55 +0530

    Added controller for Product: ProductController

---
+++ Yummy/src/main/java/com/esd/Yummy/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.esd.Yummy.controller;
+
+import com.esd.Yummy.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+
+    @GetMapping("/top-2")
+    public List<Product> getProductsByPriceRange() {
+        return productService.getTop2Products();
+    }
+}
--- Yummy/src/main/java/com/esd/Yummy/repo/ProductRepo.java
+++ Yummy/src/main/java/com/esd/Yummy/repo/ProductRepo.java
@@ -1,5 +1,6 @@
 package com.esd.Yummy.repo;
 
+import com.esd.Yummy.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
