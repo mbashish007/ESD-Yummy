62cbc398411f8fad9b7325ea8375f54fe70959b1
M B Ashish <mbashish007@gmail.com>
2024-11-20 23:57:39 +0530

    Created Entity Product

---
+++ Yummy/src/main/java/com/esd/Yummy/entity/Product.java
@@ -0,0 +1,24 @@
+package com.esd.Yummy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "price", nullable = false)
+    private double price;
+}
